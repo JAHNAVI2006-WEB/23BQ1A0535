2da03d1bc2b23424ce15df0b86003f26b4fe5501
JAHNAVI2006-WEB <jahnavibolla06@gmail.com>
2026-06-05 11:38:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import axios from "axios";
 
 function App() {
+  const [message, setMessage] = useState("");
+
   const register = async () => {
     try {
       const response = await axios.post(
@@ -8,25 +11,33 @@ function App() {
         {
           email: "jahnavibolla06@gmail.com",
           name: "Bolla Jahnavi",
-          mobileNo: "9392475593",
+          mobileNumber: "9392475593",
           githubUsername: "JAHNAVI2006-WEB",
           rollNo: "23bq1a0535",
           accessCode: "QQdEYy"
         }
       );
 
-      console.log(response.data);
-      alert("Registration Successful");
+      console.log("SUCCESS:", response.data);
+      setMessage("Registration Successful");
     } catch (error) {
-      console.error(error);
-      alert("Registration Failed");
+      console.log("ERROR:", error);
+      console.log("STATUS:", error.response?.status);
+      console.log("DATA:", error.response?.data);
+
+      setMessage("Registration Failed");
     }
   };
 
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Affordmed Registration</h1>
-      <button onClick={register}>Register</button>
+
+      <button onClick={register}>
+        Register
+      </button>
+
+      <p>{message}</p>
     </div>
   );
 }
